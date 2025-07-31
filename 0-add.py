f1604fe069f409f3608904a7129a2d7cc975e0f9
clementgibot25 <10500@holbertonstudents.com>
2025-07-31 10:42:50 +0200

    moved file

--- 0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""This module define a function to add two float numbers.
-"""
-
-
-def add(a: float, b: float) -> float:
-    """Return the sum of two float numbers.
-    """
-    return a + b
