b065d589431b53041f5db8b6be30cb0d07c5a1c3
clementgibot25 <10500@holbertonstudents.com>
2025-07-31 10:40:53 +0200

    created repo, added readme and first task

---
+++ 0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module define a function to add two float numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers.
+    """
+    return a + b
