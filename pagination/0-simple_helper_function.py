cdf5006b2da1393bdbb558cabe9f4f547eee4731
clementgibot25 <10500@holbertonstudents.com>
2025-08-11 16:56:21 +0200

    fixed pycodestyle error

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,6 @@
     two integer arguments page and page_size"""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
