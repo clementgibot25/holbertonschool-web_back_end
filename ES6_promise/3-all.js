b5d46a3906938c8dea90fb69c99eb740531240cf
clementgibot25 <10500@holbertonstudents.com>
2025-08-27 14:28:35 +0200

    Add profile signup handler with user creation and photo upload

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,15 +1,15 @@
-import {createUser, uploadPhoto} from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    return Promise.all([createUser(), uploadPhoto()])
-        .then((values) => {
-            console.log(
-                `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
-            );
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`,
+      );
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
 
 export default handleProfileSignup;
