fd12337446f46c224796b61b1cf606b242a96bd6
clementgibot25 <10500@holbertonstudents.com>
2025-09-01 13:52:31 +0200

    fixed linter error"

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (Input) => {
-    process.stdout.write(`Your name is: ${Input}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  process.stdout.write(`Your name is: ${Input}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
